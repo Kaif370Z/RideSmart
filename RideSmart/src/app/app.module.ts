36becb223ba2c74ad2f169b62ef9d7448161e00a
Kaif Tahir <79585814+Kaif370Z@users.noreply.github.com>
2023-11-09 17:17:58 +0000

    FirbaseDb connection setup

--- RideSmart/src/app/app.module.ts
+++ RideSmart/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"ridesmart-c1c47","appId":"1:846515714466:web:65810e53af9190a6339afb","storageBucket":"ridesmart-c1c47.appspot.com","apiKey":"AIzaSyC1MaVIpVs6ZDoOz3S2MkVLyQXq_ohJjfI","authDomain":"ridesmart-c1c47.firebaseapp.com","messagingSenderId":"846515714466","measurementId":"G-3GQN4LN4ZZ"})), provideFirestore(() => getFirestore())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- RideSmart/src/main.ts
+++ RideSmart/src/main.ts
@@ -1,6 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -10,3 +13,17 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyC1MaVIpVs6ZDoOz3S2MkVLyQXq_ohJjfI",
+    authDomain: "ridesmart-c1c47.firebaseapp.com",
+    projectId: "ridesmart-c1c47",
+    storageBucket: "ridesmart-c1c47.appspot.com",
+    messagingSenderId: "846515714466",
+    appId: "1:846515714466:web:65810e53af9190a6339afb",
+    measurementId: "G-3GQN4LN4ZZ"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  export const analytics = getAnalytics(app);
