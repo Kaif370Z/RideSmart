fee672749569a3882740200dbcb6cd02e1afb5fb
JakubP56 <jprochnicki56@gmail.com>
2023-11-23 22:38:56 +0000

    Android App Working and Emulating. UI Changes

--- RideSmart/src/environments/environment.prod.ts
+++ RideSmart/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC1MaVIpVs6ZDoOz3S2MkVLyQXq_ohJjfI",
+    authDomain: "ridesmart-c1c47.firebaseapp.com",
+    projectId: "ridesmart-c1c47",
+    storageBucket: "ridesmart-c1c47.appspot.com",
+    messagingSenderId: "846515714466",
+    appId: "1:846515714466:web:65810e53af9190a6339afb",
+    measurementId: "G-3GQN4LN4ZZ" }
 };
