041d7641a34a7f89b4aacd818eebbe708e166cd2
Kaif Tahir <79585814+Kaif370Z@users.noreply.github.com>
2023-12-08 02:01:53 +0000

    Added geolocation Test into acceleration tab (temp)

--- RideSmart/src/app/accelerationTab/accelerationTab.page.ts
+++ RideSmart/src/app/accelerationTab/accelerationTab.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+
 
 @Component({
   selector: 'app-accelerationTab',
@@ -9,4 +11,27 @@ export class accelerationTabPage {
 
   constructor() {}
 
+  async getCurrentLocation() {
+   try {
+     const permissionStatus = await Geolocation.checkPermissions();
+     console.log('permission status: ', permissionStatus.location);
+     if(permissionStatus?.location != 'granted') {
+       const requestStatus = await Geolocation.requestPermissions();
+       if(requestStatus.location != 'granted') {
+        // go to location settings
+        return ;
+       }
+     }
+     let options: PositionOptions = {
+      maximumAge: 3000,
+      timeout: 10000,
+      enableHighAccuracy: false
+     };
+     const position = await Geolocation.getCurrentPosition(options);
+     console.log(position);
+   } catch(e) {
+     console.log(e);
+     throw(e);
+   }
+  }
 }
