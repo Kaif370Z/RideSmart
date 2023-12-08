91301264f3eb06c9c41d0b3a362163aaaabb8559
JakubP56 <jprochnicki56@gmail.com>
2023-12-08 05:18:13 +0000

    Working on Gyroscope plugin

--- RideSmart/src/app/driveTab/driveTab.page.ts
+++ RideSmart/src/app/driveTab/driveTab.page.ts
@@ -2,6 +2,7 @@
 import { Component, Inject } from '@angular/core';
 import { PluginListenerHandle } from '@capacitor/core';
 import { Motion } from '@capacitor/motion';
+import { Platform } from '@ionic/angular';
 
 import { DeviceMotion, DeviceMotionAccelerationData, DeviceMotionAccelerometerOptions } from '@ionic-native/device-motion/ngx';
 import { Gyroscope, GyroscopeOptions, GyroscopeOrientation } from '@ionic-native/gyroscope/ngx';
@@ -11,56 +12,88 @@ import { Gyroscope, GyroscopeOptions, GyroscopeOrientation } from '@ionic-native
   templateUrl: 'driveTab.page.html',
   styleUrls: ['driveTab.page.scss']
 })
-export class driveTabPage{
+export class driveTabPage {
 
-  x:string;
-  y:string;
-  z:string;
-  timestamp:string;
+  x: string;
+  y: string;
+  z: string;
+  timestamp: string;
+
+  gx: string;
+  gy: string;
+  gz: string;
+  gtimestamp: string;
 
   //identification id of the listener
   id: any;
+  gid: any;
 
-constructor(public deviceMotion: DeviceMotion ) {
-  this.x = "-";
-  this.y = "-";
-  this.z = "-";
-  this.timestamp = "-";
-}
+  constructor(public deviceMotion: DeviceMotion, public gyroscope: Gyroscope,private platform: Platform ) {
+    this.x = "-";
+    this.y = "-";
+    this.z = "-";
+    this.timestamp = "-";
 
-start()
-{ 
-    try{
+    this.gx = "-";
+    this.gy = "-";
+    this.gz = "-";
+    this.gtimestamp = "-";
+
+
+  }
+
+  startAccel() {
+    try {
       //How often to collect accelerometer Data
-      var option: DeviceMotionAccelerometerOptions = 
+      var option: DeviceMotionAccelerometerOptions =
       {
         //200 ms
         frequency: 200
       };
 
       //
-      this.id = this.deviceMotion.watchAcceleration(option).subscribe((acc: DeviceMotionAccelerationData) =>
-      {
+      this.gid= this.deviceMotion.watchAcceleration(option).subscribe((acc: DeviceMotionAccelerationData) => {
         //assinging data from device sensors to local variables
-        this.x = ""+ acc.x;
-        this.y = ""+ acc.y;
-        this.z = ""+ acc.z;
-        this.timestamp ="" + acc.timestamp;
+        this.x = "" + acc.x;
+        this.y = "" + acc.y;
+        this.z = "" + acc.z;
+        this.timestamp = "" + acc.timestamp;
       });
     }
     //catch error if any
     catch (error) {
-    alert("Error " + error);
+      alert("Error " + error);
+    }
   }
+  stopAccel() {
+    this.id.unsubscribe();
+  }
+
+  startGyro() {
+    this.platform.ready().then(() => {
+      try {
+        this.gyroscope.getCurrent().then((orientation: GyroscopeOrientation) => {
+          // Assigning data from device sensors to local variables
+          this.gx = "" + orientation.x;
+          this.gy = "" + orientation.y;
+          this.gz = "" + orientation.z;
+          this.gtimestamp = "" + orientation.timestamp;
+        }).catch((error) => {
+          console.error('Error getting gyroscope data:', error);
+          alert('Error ' + error);
+        });
+      } catch (error) {
+        console.error('Error starting gyroscope:', error);
+        alert('Error ' + error);
+      }
+    });
+  }
+
+  stopGyro(){
+    this.gid.unsubscribe();
+  }
+
 }
 
-stop()
-{
-  this.id.unsubscribe();
-}
 
-
-}
-
-  
 
