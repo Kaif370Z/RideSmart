60d9df7d2630f5d305fe7911ff51788393dc04fb
JakubP56 <jprochnicki56@gmail.com>
2024-02-28 09:48:46 +0000

    Displaying Gyroscope Sensor Readings

--- RideSmart/src/app/driveTab/driveTab.page.ts
+++ RideSmart/src/app/driveTab/driveTab.page.ts
@@ -24,9 +24,18 @@ export class driveTabPage {
   gz: string;
   gtimestamp: string;
 
-  //identification id of the listener
+  //identification id of the listeners
   id: any;
   gid: any;
+
+  //angular velocity around y axis
+  gyroscopeRateY: number = 0;
+  
+  //lean angle estimation
+  leanAngle: number = 0;
+ 
+  //time tracking for gyroscope
+  lastUpdate: number = 0;
 
   constructor(public deviceMotion: DeviceMotion, public gyroscope: Gyroscope,private platform: Platform ) {
     this.x = "-";
@@ -38,8 +47,6 @@ export class driveTabPage {
     this.gy = "-";
     this.gz = "-";
     this.gtimestamp = "-";
-
-
   }
 
   startAccel() {
@@ -70,20 +77,56 @@ export class driveTabPage {
   }
 
   startGyro() {
-    let options: GyroscopeOptions = { frequency: 1000 }; // Set frequency to 1000ms (1 second)
+    //How often to collect accelerometer Data
+    let options: GyroscopeOptions = { frequency: 1000 }; 
     this.gid = this.gyroscope.watch(options).subscribe((orientation: GyroscopeOrientation) => {
-      this.gx = "" + orientation.x;
+      /*this.gx = "" + orientation.x;
       this.gy = "" + orientation.y;
       this.gz = "" + orientation.z;
-      this.gtimestamp = "" + orientation.timestamp;
-    });
+      this.gtimestamp = "" + orientation.timestamp;*/
+
+      //current time
+      const now = Date.now();
+      if (this.lastUpdate !== 0) {
+        //converting to seconds
+        const deltaTime = (now - this.lastUpdate) / 1000;
+        //assigning sensor data to variable 
+        this.gyroscopeRateY = orientation.y;
+        
+        this.leanAngle += this.gyroscopeRateY * deltaTime;
+      }
+      this.lastUpdate = now;
+      console.log('Current Lean Angle:', this.leanAngle);
+    });  
+  }
+
+  getLeanAngle() : number {
+    return this.leanAngle;
   }
 
   stopGyro() {
     this.gid.unsubscribe();
   }
 
+  calculateLeanAngle(): number {
+      //convert string accelerometer values to numbers
+      const ax = parseFloat(this.x);
+      const ay = parseFloat(this.y);
+      const az = parseFloat(this.z);
+
+      //convert to radians
+      const leanAngleRadians = Math.atan2(ay, Math.sqrt(ax * ax + az * az));
+      
+      //radians to degrees
+      let leanAngleDegrees = leanAngleRadians * (180 / Math.PI);
+      
+      console.log("Lean Angle Degrees:", leanAngleDegrees);
+    
+      return 90 - leanAngleDegrees;
+  }
 }
+
+
 
 
 
