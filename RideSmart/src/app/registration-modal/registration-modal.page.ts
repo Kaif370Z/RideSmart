bbb5f4cd8296668619471d56d1d572df5dff9ff6
JakubP56 <jprochnicki56@gmail.com>
2024-03-12 20:14:05 +0000

    Registration Modal working and saving to firebase auth.

--- RideSmart/src/app/registration-modal/registration-modal.page.ts
+++ RideSmart/src/app/registration-modal/registration-modal.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, LoadingController, AlertController } from '@ionic/angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-registration-modal',
@@ -7,11 +12,61 @@ import { ModalController, LoadingController, AlertController } from '@ionic/angu
   styleUrls: ['./registration-modal.page.scss'],
 })
 export class RegistrationModalPage implements OnInit {
+  credentials: FormGroup;
 
-  constructor(private modalController: ModalController) { }
+  constructor(
+
+    private fb : FormBuilder,
+    private modalController: ModalController,
+    private loadingController: LoadingController,
+    private authService: AuthService,
+    private alertController: AlertController,
+    private router: Router
+    ) {
+
+    this.credentials = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+   }
 
   ngOnInit() {
   }
+
+  get email() {
+    return this.credentials.get('email');
+  }
+
+  get password() {
+    return this.credentials.get('password');
+  }
+
+  async register() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const user = await this.authService.register(this.credentials.value);
+    await loading.dismiss();
+
+    if(user){
+      //this.router.navigateByUrl('/tabs/driveTab', {replaceUrl:true});
+      this.modalController.dismiss();
+      this.showAlert('Registration Successful', 'Log in!');
+    }
+    else{
+      this.showAlert('Registration failed', 'Please try again');
+    }
+  }
+
+  async showAlert(header: string , message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
 
   dismissModal() {
     this.modalController.dismiss();
